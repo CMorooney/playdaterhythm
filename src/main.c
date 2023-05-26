cf779952e373fdc92403a6212c798c0ce9e697f4
calvin <calvin@calvins-MacBook-Pro-2.local>
2023-05-26 12:58:29 -0400

    basic noise gen

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "pd_api.h"
 
 const char * fontpath = "/System/Fonts/Asheville-Sans-14-Bold.pft";
 
-static PlaydateAPI *pd = NULL;
+static PlaydateAPI * pd = NULL;
+static PDSynth * synth = NULL;
 static LCDFont * font = NULL;
+
+static int update(void * userdata);
+void initFont(void);
+void initSynth(void);
 
 int eventHandler(PlaydateAPI * playdate, PDSystemEvent event, uint32_t arg) {
   (void)arg; // arg is currently only used for event = kEventKeyPressed
@@ -18,16 +22,33 @@ int eventHandler(PlaydateAPI * playdate, PDSystemEvent event, uint32_t arg) {
     pd -> display -> setRefreshRate(0);
     pd -> system -> setUpdateCallback(update, pd);
 
-    // load font
-    const char * err;
-    font = pd -> graphics -> loadFont(fontpath, & err);
-    if (font == NULL) {
-      pd -> system -> error("%s:%i Couldn't load font %s: %s", __FILE__, __LINE__, fontpath, err);
-    }
-
+    initFont();
+    initSynth();
   }
 
   return 0;
+}
+
+void initFont(void) {
+  const char * err;
+  font = pd -> graphics -> loadFont(fontpath, & err);
+  if (font == NULL) {
+    pd -> system -> error("%s:%i Couldn't load font %s: %s", __FILE__, __LINE__, fontpath, err);
+  }
+}
+
+void initSynth(void) {
+  pd -> system -> logToConsole("init synth");
+  synth = pd -> sound -> synth -> newSynth();
+  if (synth == NULL) {
+    pd -> system -> error("Couldn't create synth");
+  } else {
+    pd -> sound -> synth -> setWaveform(synth, kWaveformNoise);
+    pd -> sound -> synth -> setAttackTime(synth, 0);
+    pd -> sound -> synth -> setDecayTime(synth, 10);
+    pd -> sound -> synth -> playNote(synth, 440, 1, -1, 0);
+    pd -> system -> logToConsole("playing note");
+  }
 }
 
 static int update(void * userdata) {
