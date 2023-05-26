973d18a0a162f382171694868c4bfe613a980ba1
calvin <calvin@calvins-MacBook-Pro-2.local>
2023-05-26 12:58:20 -0400

    build fixes in cmake

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/calvin/repos/HelloWorldPlaydate
+# Install script for directory: /Users/calvin/repos/PlaydateRhythm
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/calvin/repos/HelloWorldPlaydate/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/calvin/repos/PlaydateRhythm/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
